682701441ca6158361564b9499a017113fa14b45
Curt Lawson <curt.lawson@computer.org>
2020-08-21 18:48:57 +0000

    Added week_2 files.

---
+++ week_2/fig02_01.c
@@ -0,0 +1,18 @@
+// ----------------------------------------------------------------------------
+// Filename: fig02_01.c
+//
+// Purpose : This is an introductory "hello world" type program to introduce
+//           the students to C for the first time and highlight a few
+//           important language features.
+//
+//           This program was based on the one shown in Fig. 2.1 in the text.
+//
+// Author  : Curt Lawson
+// ----------------------------------------------------------------------------
+#include <stdio.h>
+
+// function main begins program execution
+int main(void)
+{
+    printf("Welcome to C!\n");
+} // end function main
